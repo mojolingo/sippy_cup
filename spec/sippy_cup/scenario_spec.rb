0c5d2c5abad6ee41edb9a98cf6268c12cc5b79a9
Ben Klang <bklang@mojolingo.com>
2013-08-08 15:11:10 -0400

    Add more specs for Scenario generation

--- spec/sippy_cup/scenario_spec.rb
+++ spec/sippy_cup/scenario_spec.rb
@@ -1,8 +1,30 @@
 require 'spec_helper'
 
 describe SippyCup::Scenario do
+  let(:default_args) { {source: '127.0.0.1:5060', destination: '10.0.0.1:5080'} }
+
+  it %q{should create a media stream on initialization} do
+    SippyCup::Media.should_receive(:new).once.with('127.0.0.1', '5060', '127.0.0.2', '5061')
+    SippyCup::Scenario.new 'Test', source: '127.0.0.1:5060', destination: '127.0.0.2:5061'
+  end
+
+  it %q{should take a block to generate a scenario} do
+    s = SippyCup::Scenario.new 'Test', default_args do
+      invite
+    end
+
+    s.to_xml.should =~ %r{INVITE sip:\[service\]@\[remote_ip\]:\[remote_port\] SIP/2.0}
+  end
+
+  it %q{should allow creating a blank scenario with no block} do
+    s = SippyCup::Scenario.new 'Test', default_args
+    s.invite
+    s.to_xml.should =~ %r{INVITE sip:\[service\]@\[remote_ip\]:\[remote_port\] SIP/2.0}
+  end
+
   describe '#wait_for_answer' do
     let(:scenario) { scenario = SippyCup::Scenario.new 'Test', source: '127.0.0.1:5061', destination: '127.0.0.1:5060' }
+
     it %q{should tell SIPp to optionally receive a SIP 100, 180 and 183 by default, while requiring a 200} do
       scenario.wait_for_answer
 
