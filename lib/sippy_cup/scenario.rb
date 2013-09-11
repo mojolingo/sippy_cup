4d112f35eb2912f5f5713d5a1cd123f956136bda
Will Drexler <wdrexler@mojolingo.com>
2013-09-11 12:37:48 -0400

    Move headers to the end of the SIP INVITE

--- lib/sippy_cup/scenario.rb
+++ lib/sippy_cup/scenario.rb
@@ -56,7 +56,6 @@ module SippyCup
     def invite(opts = {})
       opts[:retrans] ||= 500
       headers = opts.delete :headers
-      headers << "\n        " if headers
       rtp_string = @rtcp_port ? "m=audio #{@rtcp_port.to_i - 1} RTP/AVP 0 101\na=rtcp:#{@rtcp_port}\n" : "m=audio [media_port] RTP/AVP 0 101\n"
       # FIXME: The DTMF mapping (101) is hard-coded. It would be better if we could
       # get this from the DTMF payload generator
@@ -64,7 +63,7 @@ module SippyCup
 
         INVITE sip:[service]@[remote_ip]:[remote_port] SIP/2.0
         Via: SIP/2.0/[transport] [local_ip]:[local_port];branch=[branch]
-        #{headers}From: "#{@from_user}" <sip:#{@from_user}@[local_ip]>;tag=[call_number]
+        From: "#{@from_user}" <sip:#{@from_user}@[local_ip]>;tag=[call_number]
         To: <sip:[service]@[remote_ip]:[remote_port]>
         Call-ID: [call_id]
         CSeq: [cseq] INVITE
@@ -73,6 +72,7 @@ module SippyCup
         User-Agent: #{USER_AGENT}
         Content-Type: application/sdp
         Content-Length: [len]
+        #{headers}
 
         v=0
         o=user1 53655765 2353687637 IN IP[local_ip_type] [local_ip]
