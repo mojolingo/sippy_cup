dad7c63cd88c1758f9d2cbe356daa413273d17fb
Ben Langfeld <ben@langfeld.me>
2013-09-12 22:14:36 -0300

    #prepare_command should not be part of Runner's public interface

--- lib/sippy_cup/runner.rb
+++ lib/sippy_cup/runner.rb
@@ -35,22 +35,6 @@ module SippyCup
       scenario.compile!
     end
 
-    def prepare_command
-      command = "sudo sipp"
-      source_port = @options[:source_port] || '8836'
-      sip_user = @options[:sip_user] || '1'
-      command << " -i #{@options[:source]} -p #{source_port} -sf #{File.expand_path @options[:scenario]}.xml"
-      command << " -l #{@options[:max_concurrent]} -m #{@options[:number_of_calls]} -r #{@options[:calls_per_second]}"
-      command << " -s #{sip_user}"
-      if @options[:stats_file]
-        stats_interval = @options[:stats_interval] || 1
-        command << " -trace_stat -stf #{@options[:stats_file]} -fd #{stats_interval}"
-      end
-      command << " -inf #{@options[:scenario_variables]}" if @options[:scenario_variables]
-      command << " #{@options[:destination]}"
-      command
-    end
-
     # Runs the loaded scenario using SIPp
     #
     # @raises Errno::ENOENT when the SIPp executable cannot be found
@@ -113,6 +97,22 @@ module SippyCup
     end
 
   private
+
+    def prepare_command
+      command = "sudo sipp"
+      source_port = @options[:source_port] || '8836'
+      sip_user = @options[:sip_user] || '1'
+      command << " -i #{@options[:source]} -p #{source_port} -sf #{File.expand_path @options[:scenario]}.xml"
+      command << " -l #{@options[:max_concurrent]} -m #{@options[:number_of_calls]} -r #{@options[:calls_per_second]}"
+      command << " -s #{sip_user}"
+      if @options[:stats_file]
+        stats_interval = @options[:stats_interval] || 1
+        command << " -trace_stat -stf #{@options[:stats_file]} -fd #{stats_interval}"
+      end
+      command << " -inf #{@options[:scenario_variables]}" if @options[:scenario_variables]
+      command << " #{@options[:destination]}"
+      command
+    end
 
     def process_exit_status(process_status, error_message = nil)
       exit_code = process_status[1].exitstatus
