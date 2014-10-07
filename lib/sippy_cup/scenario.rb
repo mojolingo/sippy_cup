905edf605b9cf58ce4437a8fc9784fd02331dc58
Ben Klang <bklang@mojolingo.com>
2014-10-06 23:57:55 -0400

    Helpers for UAS mode
    * send_trying (alias send_100)
    * send_ringing (alias send_180)
    * send_answer
    * receive_ack
    * answer (send_answer + receive_ack)

--- lib/sippy_cup/scenario.rb
+++ lib/sippy_cup/scenario.rb
@@ -201,6 +201,100 @@ a=fmtp:101 0-15
     end
 
     #
+    # Expect to receive a SIP INVITE
+    #
+    # @param [Hash] opts A set of options containing SIPp <recv> element attributes
+    #
+    def receive_invite(opts = {})
+      recv opts.merge request: 'INVITE'
+    end
+
+    #
+    # Send a "100 Trying" response
+    #
+    # @param [Hash] opts A set of options containing SIPp <recv> element attributes
+    #
+    def send_trying(opts = {})
+      msg = <<-MSG
+
+SIP/2.0 100 Trying
+[last_Via:]
+[last_From:]
+[last_To:];tag=[call_number]
+[last_Call-ID:]
+[last_CSeq:]
+Server: #{USER_AGENT}
+Contact: <sip:[local_ip]:[local_port];transport=[transport]>
+Content-Length: 0
+      MSG
+      send msg, opts
+    end
+    alias :send_100 :send_trying
+
+    #
+    # Send a "180 Ringing" response
+    #
+    # @param [Hash] opts A set of options containing SIPp <recv> element attributes
+    #
+    def send_ringing(opts = {})
+      msg = <<-MSG
+
+SIP/2.0 180 Ringing
+[last_Via:]
+[last_From:]
+[last_To:];tag=[call_number]
+[last_Call-ID:]
+[last_CSeq:]
+Server: #{USER_AGENT}
+Contact: <sip:[local_ip]:[local_port];transport=[transport]>
+Content-Length: 0
+      MSG
+      send msg, opts
+    end
+    alias :send_180 :send_ringing
+
+    #
+    # Answer an incoming call
+    #
+    def send_answer(opts = {})
+      opts[:retrans] ||= DEFAULT_RETRANS
+      msg = <<-MSG
+
+SIP/2.0 200 Ok
+[last_Via:]
+[last_From:]
+[last_To:];tag=[call_number]
+[last_Call-ID:]
+[last_CSeq:]
+Server: #{USER_AGENT}
+Contact: <sip:[local_ip]:[local_port];transport=[transport]>
+Content-Type: application/sdp
+Content-Length: [len]
+
+v=0
+o=user1 53655765 2353687637 IN IP[local_ip_type] [local_ip]
+s=-
+c=IN IP[media_ip_type] [media_ip]
+t=0 0
+m=audio [media_port] RTP/AVP 0
+a=rtpmap:0 PCMU/8000
+      MSG
+      send msg, opts
+    end
+
+    #
+    # Helper method to answer an INVITE and expect the ACK
+    #
+    def answer(opts = {})
+      send_answer opts
+      receive_ack opts
+    end
+
+    def receive_ack(opts = {})
+      recv opts.merge request: 'ACK'
+    end
+
+    #
     # Sets an expectation for a SIP 100 message from the remote party
     #
     # @param [Hash] opts A set of options to modify the expectation
