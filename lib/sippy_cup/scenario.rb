mojolingo/sippy_cup

commit 893865f3d65e2f89ac9127c8457452fc6f66f0da

WIP

lib/sippy_cup/scenario.rb
@@ -104,7 +104,7 @@ module SippyCup
       @scenario_options = args.merge name: name
       @filename = args[:filename] || name.downcase.gsub(/\W+/, '_')
       @filename = File.expand_path @filename, Dir.pwd
-      @media = Media.new '127.0.0.255', 55555, '127.255.255.255', 5060
+      @media = nil
       @message_variables = 0
       # Reference variables don't generate warnings/errors if unused in the scenario
       @reference_variables = Set.new
@@ -314,6 +314,7 @@ SIP/2.0 200 Ok
 Server: #{USER_AGENT}
 Contact: <sip:[local_ip]:[local_port];transport=[transport]>
 Content-Type: application/sdp
+[routes]
 Content-Length: [len]
 
 v=0
@@ -324,11 +325,14 @@ t=0 0
 m=audio [media_port] RTP/AVP 0
 a=rtpmap:0 PCMU/8000
       MSG
+      start_media
       send msg, opts
     end
 
     #
     # Helper method to answer an INVITE and expect the ACK
+    #
+    # @param [Hash] opts A set of options containing SIPp element attributes - will be passed to both the <send> and <recv> elements
     #
     def answer(opts = {})
       send_answer opts
@@ -443,7 +447,7 @@ Content-Length: 0
     def sleep(seconds)
       milliseconds = (seconds.to_f * MSEC).to_i
       pause milliseconds
-      @media << "silence:#{milliseconds}"
+      @media << "silence:#{milliseconds}" if @media
     end
 
     #
@@ -458,6 +462,7 @@ Content-Length: 0
     #   send_digits '1234'
     #
     def send_digits(digits)
+      raise "Media not started" unless @media
       delay = (0.250 * MSEC).to_i # FIXME: Need to pass this down to the media layer
       digits.split('').each do |digit|
         raise ArgumentError, "Invalid DTMF digit requested: #{digit}" unless VALID_DTMF.include? digit
@@ -648,7 +653,7 @@ Content-Length: 0
     #   scenario.compile! # Leaves files at test_scenario.xml and test_scenario.pcap
     #
     def compile!
-      unless @media.empty?
+      unless @media.nil?
         print "Compiling media to #{@filename}.pcap..."
         compile_media.to_file filename: "#{@filename}.pcap"
         puts "done."
@@ -736,6 +741,7 @@ Content-Length: 0
     end
 
     def compile_media
+      raise "Media not started" unless @media
       @media.compile!
     end
 
@@ -776,6 +782,7 @@ Content-Length: 0
     end
 
     def start_media
+      @media = Media.new '127.0.0.255', 55555, '127.255.255.255', 44444
       nop = doc.create_element('nop') { |nop|
         nop << doc.create_element('action') { |action|
           action << doc.create_element('exec')

.gitignore
@@ -5,3 +5,6 @@
 Gemfile.lock
 .ruby-gemset
 tmp/
+/examples/*.xml
+/examples/*.pcap
+/examples/*.log

examples/navigate_ivr.yml
@@ -1,0 +1,14 @@
+source: 192.0.2.15
+destination: 192.0.2.200
+max_concurrent: 10
+calls_per_second: 5
+number_of_calls: 20
+steps:
+  - invite
+  - wait_for_answer
+  - ack_answer
+  - sleep 3
+  - send_digits '3125551234'
+  - sleep 5
+  - send_digits '#'
+  - wait_for_hangup

examples/simple_call.yml
@@ -1,0 +1,11 @@
+source: 192.0.2.15
+destination: 192.0.2.200
+max_concurrent: 10
+calls_per_second: 5
+number_of_calls: 20
+steps:
+  - invite
+  - wait_for_answer
+  - ack_answer
+  - sleep 5
+  - hangup

commit dedfc0da8127d47b196f9029c370e5ad440f5ca9

Fix non-writing of PCAP file when media is empty
This was broken by 893865f3d65e2f89ac9127c8457452fc6f66f0da.

lib/sippy_cup/scenario.rb
@@ -2,6 +2,7 @@
 require 'nokogiri'
 require 'psych'
 require 'active_support/core_ext/hash'
+require 'active_support/core_ext/object/blank'
 require 'tempfile'
 require 'set'
 
@@ -659,7 +660,7 @@ Content-Length: 0
       @media_nodes.reverse.each do |nop|
         nopdup = docdup.xpath(nop.path)
 
-        if pcap_path.nil? or @media.empty?
+        if pcap_path.nil? or @media.blank?
           nopdup.remove
         else
           exec = nopdup.xpath("./action/exec").first
@@ -694,7 +695,7 @@ Content-Length: 0
     #   scenario.compile! # Leaves files at test_scenario.xml and test_scenario.pcap
     #
     def compile!
-      unless @media.nil?
+      unless @media.blank?
         print "Compiling media to #{@filename}.pcap..."
         compile_media.to_file filename: "#{@filename}.pcap"
         puts "done."
@@ -720,7 +721,7 @@ Content-Length: 0
     # @see http://www.ruby-doc.org/stdlib-1.9.3/libdoc/tempfile/rdoc/Tempfile.html
     #
     def to_tmpfiles
-      unless @media.nil? || @media.empty?
+      unless @media.blank?
         media_file = Tempfile.new 'media'
         media_file.binmode
         media_file.write compile_media.to_s